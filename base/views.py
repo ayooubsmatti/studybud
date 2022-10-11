8fa3e85be67b06d60133c57889665f3138dcaf61
saber ayoub smatti <ayooubsmatti@gmail.com>
2022-10-11 00:52:38 -0400

    base commit with simple template inheritance

--- base/views.py
+++ base/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+
+rooms= [
+    {'id':1,'name':'Lets learn python!'},
+    {'id':2,'name':'Lets devoloper'},
+    {'id':3,'name':'Lets learn css!'},
+]
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 def room(request):
-    return render(request, 'room.html')
+    context = {'rooms':rooms}
+    return render(request, 'room.html',context)
