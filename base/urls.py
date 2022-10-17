d8116617604e8f34b8adf4ce720e361e951c7ed5
saber ayoub smatti <ayooubsmatti@gmail.com>
2022-10-16 22:21:29 -0400

    User Profile Page

--- base/urls.py
+++ base/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('logout/', views.LogoutUser, name="logout"),
     
     path('register/', views.registerPage, name="register"),
+    
+    path('profile/<str:pk>/', views.userProfile, name="user-profile"),
 
     path('room/<str:pk>/', views.room, name="room"),
     path('create-room/', views.createRoom, name="create-room"),
