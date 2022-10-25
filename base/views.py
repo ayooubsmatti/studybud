ca11952e2cbeb33b4579392a17db04c6ff37a0fb
saber ayoub smatti <ayooubsmatti@gmail.com>
2022-10-25 03:18:27 -0400

    static file ----- basic form editing style part 2

--- base/views.py
+++ base/views.py
@@ -99,6 +99,7 @@ def userProfile(request,pk):
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
@@ -107,14 +108,14 @@ def createRoom(request):
             room.save()
             return redirect('home')
 
-    context = {'form':form}
+    context = {'form':form,'topics':topics}
     return render(request,'base/room_form.html',context)
 
 @login_required(login_url='login')
 def updateRoom(request,pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
-
+    topics = Topic.objects.all()
     
     if request.user != room.host:
         return HttpResponse("you are not allowed here !!")
@@ -125,7 +126,7 @@ def updateRoom(request,pk):
             form.save()
             return redirect('home')
     
-    context = {'form':form}
+    context = {'form':form,'topics':topics}
     return render(request, 'base/room_form.html',context)
 
 @login_required(login_url='login')
